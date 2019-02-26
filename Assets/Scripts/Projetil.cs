dcb95487e6413c5d03a642bdc2801f690f90d6b2
Paulo Salvatore <paulo@salvatore.tech>
2019-02-26 11:59:43 -0300

    Criação do projeto

---
+++ Assets/Scripts/Projetil.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetil : MonoBehaviour
+{
+    [Range(1, 5)]
+    public float velocidade = 2f;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * velocidade * Time.deltaTime);
+    }
+}
