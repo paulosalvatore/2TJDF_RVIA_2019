dbd21ab30cd68f9f370940c91692fa71bb225ae1
Paulo Salvatore <paulo@salvatore.tech>
2019-04-02 12:22:23 -0300

    Projeto finalizado

--- Assets/Scripts/GeradorZumbis.cs
+++ Assets/Scripts/GeradorZumbis.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeradorZumbis : MonoBehaviour {
+public class GeradorZumbis : MonoBehaviour
+{
+    public float delay = 2f;
 
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject zumbiPrefab;
+    public float area = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("GerarZumbi", delay, delay);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void GerarZumbi()
+    {
+        GameObject zumbi = Instantiate(zumbiPrefab);
+
+        Vector2 posicaoAleatoria = Random.insideUnitCircle * area;
+        zumbi.transform.position = new Vector3(
+            posicaoAleatoria.x,
+            zumbi.transform.position.y,
+            posicaoAleatoria.y
+        );
 	}
 }
--- Assets/Scripts/Zumbi.cs
+++ Assets/Scripts/Zumbi.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Zumbi : MonoBehaviour {
 
@@ -9,15 +10,26 @@ public class Zumbi : MonoBehaviour {
 
     public float velocidade = 0.35f;
 
+    bool morto = false;
+
+    GameObject jogador;
+
     // Use this for initialization
     void Start () {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
+
+        jogador = GameObject.FindGameObjectWithTag("Player");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        rb.velocity = transform.forward * velocidade;
+
+    // Update is called once per frame
+    void Update () {
+        if (!morto)
+        {
+            rb.velocity = transform.forward * velocidade;
+
+            transform.LookAt(jogador.transform);
+        }
 	}
 
     void OnTriggerEnter(Collider other)
@@ -26,15 +38,31 @@ public class Zumbi : MonoBehaviour {
         print("OnTriggerEnter Zumbi: " + other.tag);
 
         // Verifico se a tag do trigger é igual a "Projétil"
-        if (other.CompareTag("Projétil"))
+        if (!morto && other.CompareTag("Projétil"))
         {
             // Objeto other verificado. É o projétil
             // Destrói o gameObject do projétil
             Destroy(other.gameObject);
 
-            // Destrói o zumbi que foi atingido
-            //Destroy(gameObject);
-            anim.SetTrigger("Morrer");
+            Morrer();
         }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.buildIndex);
+        }
+    }
+
+    private void Morrer()
+    {
+        // Destrói o zumbi que foi atingido
+        //Destroy(gameObject);
+        anim.SetTrigger("Morrer");
+
+        morto = true;
     }
 }
